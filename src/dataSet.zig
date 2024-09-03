89af21fdc38e839d0254d66f06a82b4ab56edda8
jklw10 <112possu@gmail.com>
2024-09-03 14:06:54 +0300

    Removed dregs of last change

--- src/dataSet.zig
+++ src/dataSet.zig
@@ -79,33 +79,3 @@ pub fn readIdxFile(path: []const u8, skip_bytes: u8, allocator: std.mem.Allocato
     );
     return data;
 }
-
-pub fn readMnist(allocator: std.mem.Allocator) !Data {
-    const train_images_path: []const u8 = "data/train-images.idx3-ubyte";
-    const train_images_u8 = try readIdxFile(train_images_path, 16, allocator);
-    defer allocator.free(train_images_u8);
-    var train_images = try allocator.alloc(f64, 784 * 60000);
-    var i: u32 = 0;
-    while (i < 784 * 60000) : (i += 1) {
-        const x: f64 = @as(f64, @floatFromInt(train_images_u8[i]));
-        train_images[i] = x / 255;
-    }
-
-    const train_labels_path: []const u8 = "data/train-labels.idx1-ubyte";
-    const train_labels = try readIdxFile(train_labels_path, 8, allocator);
-
-    const test_images_path: []const u8 = "data/t10k-images.idx3-ubyte";
-    const test_images_u8 = try readIdxFile(test_images_path, 16, allocator);
-    defer allocator.free(test_images_u8);
-    var test_images = try allocator.alloc(f64, 784 * 10000);
-    i = 0;
-    while (i < 784 * 10000) : (i += 1) {
-        const x: f64 = @as(f64, @floatFromInt(test_images_u8[i]));
-        test_images[i] = x / 255;
-    }
-
-    const test_labels_path: []const u8 = "data/t10k-labels.idx1-ubyte";
-    const test_labels = try readIdxFile(test_labels_path, 8, allocator);
-
-    return Data{ .train_images = train_images, .train_labels = train_labels, .test_images = test_images, .test_labels = test_labels };
-}
